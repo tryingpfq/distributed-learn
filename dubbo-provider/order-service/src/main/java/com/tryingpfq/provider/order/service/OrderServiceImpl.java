8acff8c5c23a2197ef9beba7d32febc030f0f83d
tryingpfq <tryingpfq@163.com>
2020-04-01 18:07:04 +0800

    feat(distributed): dubbo 注解注入provider

--- dubbo-provider/order-service/src/main/java/com/tryingpfq/provider/order/service/OrderServiceImpl.java
+++ dubbo-provider/order-service/src/main/java/com/tryingpfq/provider/order/service/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package com.tryingpfq.provider.order.service;
 
+import com.alibaba.dubbo.config.annotation.Service;
 import com.tryingpfq.provider.api.order.IOrderService;
 import com.tryingpfq.provider.api.order.OrderRequest;
 import com.tryingpfq.provider.api.order.OrderResponse;
@@ -8,6 +9,7 @@ import com.tryingpfq.provider.api.order.OrderResponse;
  * @author tryingpfq
  * @date 2020/3/30
  **/
+@Service
 public class OrderServiceImpl implements IOrderService {
     public OrderResponse order(OrderRequest request) {
         System.out.println("request is come:" + request.toString());
